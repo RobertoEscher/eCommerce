45f64cb634d271beaeb75e34f03c4b1871ba021d
Roberto Escher <robertoescher@hotmail.com>
2024-09-26 18:38:13 -0300

    Atualização 26-09

---
+++ eCommerce.Office/eCommerceOfficeContext.cs
@@ -0,0 +1,23 @@
+﻿using eCommerce.Office.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Office
+{
+    public class eCommerceOfficeContext : DbContext
+    {
+        public DbSet<Colaborador>? Colaboradores { get; set; }
+        public DbSet<Setor>? Setores { get; set; }
+        public DbSet<Turma>? Turmas { get; set; }
+        public DbSet<Veiculo>? Veiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerceOffice;Integrated Security=True;");
+        }
+    }
+}
